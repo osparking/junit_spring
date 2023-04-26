13581729c19e305032bad939d454e167ed41f4ab
osparking <jbpark03@gmail.com>
2023-04-26 14:31:40 +0900

    exact annotation used

--- src/main/java/com/example/testingweb/HomeController.java
+++ src/main/java/com/example/testingweb/HomeController.java
@@ -1,13 +1,12 @@
 package com.example.testingweb;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
-	@RequestMapping(method=RequestMethod.GET, value = "/")
+	@GetMapping("/")
 	public @ResponseBody String greeting() {
 		return "여러분 안녕?";
 	}
